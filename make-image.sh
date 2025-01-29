71a4e3a3aa699cf0473bc21f3cbb5f7deacd14c0
Robin Candau <antiz@archlinux.org>
2025-01-29 12:14:10 +0100

    Rename the script to make-image.sh In te case of WSL2, the rootfs tarball actually is the 'image'. So make-image.sh feels like a more precision name for the script

---
+++ make-image.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -euo pipefail
+
+declare -r WRAPPER="fakechroot -- fakeroot"
+declare -r DATE=$(date +"%Y.%m.%d")
+declare -r BUILDDIR="$1"
+declare -r OUTPUTDIR="$2"
+
+mkdir -vp "$BUILDDIR/alpm-hooks/usr/share/libalpm/hooks"
+find /usr/share/libalpm/hooks -exec ln -sf /dev/null "$BUILDDIR/alpm-hooks"{} \;
+
+mkdir -vp "$BUILDDIR/var/lib/pacman/" "$OUTPUTDIR"
+install -Dm 644 "/usr/share/devtools/pacman.conf.d/extra.conf" "$BUILDDIR/etc/pacman.conf"
+
+sed 's/Include = /&rootfs/g' < "$BUILDDIR/etc/pacman.conf" > pacman.conf
+
+cp --recursive --preserve=timestamps rootfs/* "$BUILDDIR/"
+ln -sf /usr/lib/os-release "$BUILDDIR/etc/os-release"
+
+$WRAPPER -- \
+    pacman -Sy -r "$BUILDDIR" \
+        --noconfirm --dbpath "$BUILDDIR/var/lib/pacman" \
+        --config pacman.conf \
+        --noscriptlet \
+        --hookdir "$BUILDDIR/alpm-hooks/usr/share/libalpm/hooks/" base
+
+$WRAPPER -- chroot "$BUILDDIR" update-ca-trust
+$WRAPPER -- chroot "$BUILDDIR" pacman-key --init
+$WRAPPER -- chroot "$BUILDDIR" pacman-key --populate
+$WRAPPER -- chroot "$BUILDDIR" /usr/bin/systemd-sysusers --root "/"
+
+# fakeroot to map the gid/uid of the builder process to root
+# See https://gitlab.archlinux.org/archlinux/archlinux-docker/-/issues/22
+fakeroot -- \
+    tar \
+        --numeric-owner \
+        --xattrs \
+        --acls \
+        --exclude-from=exclude \
+        -C "$BUILDDIR" \
+        -c . \
+        -f "$OUTPUTDIR/archlinux-$DATE.tar"
+
+cd "$OUTPUTDIR"
+zstd --long -T0 -8 "archlinux-$DATE.tar"
+mv "archlinux-$DATE.tar.zst" "archlinux-$DATE.wsl"
+sha256sum "archlinux-$DATE.wsl" > "archlinux-$DATE.wsl.SHA256"
