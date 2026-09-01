69a7f2008896c2b94452a40ec0bcfdcfad8aab31
Mark Hegreberg <mark@hegreberg.io>
2026-09-01 10:52:30 -0700

    mask units discouraged by microsoft
    microsoft CI gives warning:
    Warning:
    * Found discouraged system unit: /usr/lib/systemd/system/local-fs.target.wants/tmp.mount
    * Found discouraged system unit: /usr/lib/systemd/system/sysinit.target.wants/systemd-tmpfiles-setup-dev-early.service
    * Found discouraged system unit: /usr/lib/systemd/system/sysinit.target.wants/systemd-tmpfiles-setup-dev.service
    * Found discouraged system unit: /usr/lib/systemd/system/sysinit.target.wants/systemd-tmpfiles-setup.service

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -44,7 +44,13 @@ fakechroot -- fakeroot -- chroot "$BUILDDIR" update-ca-trust
 fakechroot -- fakeroot -- chroot "$BUILDDIR" pacman-key --init
 fakechroot -- fakeroot -- chroot "$BUILDDIR" pacman-key --populate
 fakechroot -- fakeroot -- chroot "$BUILDDIR" /usr/bin/systemd-sysusers --root "/"
-fakechroot -- fakeroot -- chroot "$BUILDDIR" /usr/bin/systemctl mask systemd-firstboot console-getty
+fakechroot -- fakeroot -- chroot "$BUILDDIR" /usr/bin/systemctl mask \
+	systemd-firstboot \
+	console-getty \
+	tmp.mount \
+	systemd-tmpfiles-setup-dev-early \
+	systemd-tmpfiles-setup-dev \
+	systemd-tmpfiles-setup
 
 # Disable getty template units to prevent service failures caused by shared Hyper-V TTY devices across WSL instances
 # See https://github.com/microsoft/WSL/issues/13595
